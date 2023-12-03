e160b4bb87635d9426b8c43a089679da18ab1124
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-12-03 17:18:15 +0300

    API Endpoints Change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //base_url: 'http://localhost:3000/',
   //base_url: 'https://53ad-197-237-73-99.ngrok-free.app/',
-  base_url: 'https://0e16-41-90-65-6.ngrok-free.app/',
+  base_url: 'https://3292-41-90-65-6.ngrok-free.app/',
   //auth_url: 'http://localhost:3001/',
-  auth_url: 'https://abd7-41-90-65-6.ngrok-free.app/',
+  auth_url: 'https://ac0b-41-90-65-6.ngrok-free.app/',
 };
