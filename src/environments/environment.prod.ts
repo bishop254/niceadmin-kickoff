a16ccca3ce5652ef5eeab61709b6fa17e05fb229
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-12-03 14:56:16 +0300

    Updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  //base_url: 'http://localhost:3000/',
+  base_url: 'https://53ad-197-237-73-99.ngrok-free.app/',
+  //auth_url: 'http://localhost:3001/',
+  auth_url: 'https://3f9e-197-237-73-99.ngrok-free.app/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3000/',
-  auth_url: 'http://localhost:3001/'
+  //base_url: 'http://localhost:3000/',
+  base_url: 'https://53ad-197-237-73-99.ngrok-free.app/',
+  //auth_url: 'http://localhost:3001/',
+  auth_url: 'https://3f9e-197-237-73-99.ngrok-free.app/',
 };
 
 /*
