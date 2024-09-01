73d5cf8a4d19dd39e0137e3ddad74e147614b074
bishop254 <karanjachiuri@gmail.com>
2024-09-01 10:08:07 +0300

    Updates on images

--- src/app/layouts/dashboardlayout.component.ts
+++ src/app/layouts/dashboardlayout.component.ts
@@ -3,20 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   template: `<app-header></app-header>
-            <app-sidebar></app-sidebar>
-            <main id="main" class="main">
-              <router-outlet></router-outlet>
-            </main>
-            <footer id="footer" class="footer mt-auto py-3">
-              <div class="copyright">
-                &copy; Copyright <strong><span>Truste Technology <a href="https://trustetechnology.com"><i class="bi bi-globe"></i></a></span></strong>. All Rights Reserved
-              </div>
-            </footer>
-            <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>`,
+    <app-sidebar></app-sidebar>
+    <main
+      style="background-image: url('./assets/img/3611852.jpg')"
+      id="main"
+      class="main"
+    >
+      <router-outlet></router-outlet>
+    </main>
+    <footer id="footer" class="footer mt-auto py-3">
+      <div class="copyright">
+        &copy; Copyright
+        <strong
+          ><span
+            >Truste Technology
+            <a href="https://trustetechnology.com"
+              ><i class="bi bi-globe"></i></a></span></strong
+        >. All Rights Reserved
+      </div>
+    </footer>
+    <a
+      href="#"
+      class="back-to-top d-flex align-items-center justify-content-center"
+      ><i class="bi bi-arrow-up-short"></i
+    ></a>`,
   styles: [
     `
       .main {
-        background-image: url('/assets/img/3611852.jpg');
+        // background-image: url('/assets/img/3611852.jpg');
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
@@ -33,14 +47,14 @@ import { Component, OnInit } from '@angular/core';
         padding: 0.5rem !important;
         border-radius: 8px !important;
 
-        ol{
-            margin-bottom: 0 !important;
+        ol {
+          margin-bottom: 0 !important;
         }
       }
     `,
   ],
 })
 export class DashboardLayoutComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void { }
+  constructor() {}
+  ngOnInit(): void {}
 }
